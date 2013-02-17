b32ccc161b89ba2250ba45468dcf716fce5cdda1
Mario Rimann <mario@rimann.org>
2013-02-18 00:14:14 +0100

    [FEATURE] Add Google Analytics code to the footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
 Footer Snippet for the Baseblog Kirby Theme
 *******************************************/
 ?>
@@ -8,6 +8,18 @@ Footer Snippet for the Baseblog Kirby Theme
   		</footer>
 
   	</div><!-- #site -->
+
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', '<?php echo $site->googleanalyticscode() ?>']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
 
 </body>
 
